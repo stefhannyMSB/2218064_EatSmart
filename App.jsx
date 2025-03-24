1dfe185dac94500208b923227142e6b85ab6c7af
stefhannyMSB <stefannyabel@gmail.com>
2025-03-24 09:37:54 +0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,364 @@
+import React from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+} from 'react-native';
+import {Element3, Receipt21, Clock, Message} from 'iconsax-react-native';
+import {fontType, colors} from './src/theme';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}> EATSMART </Text>
+        <Element3 color={colors.black()} variant="Linear" size={24} />
+      </View>
+      <View style={styles.listCategory}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={{...category.item, marginLeft: 24}}>
+            <Text style={{...category.title, color: colors.blue()}}>
+              Recomendation
+            </Text>
+          </View>
+          <View style={category.item}>
+            <Text style={category.title}>Diet Sehat</Text>
+          </View>
+          <View style={category.item}>
+            <Text style={category.title}>Flu</Text>
+          </View>
+          <View style={category.item}>
+            <Text style={category.title}>Batuk</Text>
+          </View>
+        </ScrollView>
+      </View>
+      <ListBlog />
+    </View>
+  );
+}
+const ListBlog = () => {
+  return (
+    <ScrollView>
+      <View style={styles.listBlog}>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={{gap: 15}}>
+          <View style={{...itemHorizontal.cardItem, marginLeft: 24}}>
+            <ImageBackground
+              style={itemHorizontal.cardImage}
+              resizeMode="cover"
+              imageStyle={{borderRadius: 15}}
+              source={{
+                uri: 'https://tse2.mm.bing.net/th?id=OIP.i5Yx6SWyxlzFSg_-hpg2uQHaFi&pid=Api&P=0&h=180',
+              }}>
+              <View style={itemHorizontal.cardContent}>
+                <View style={itemHorizontal.cardInfo}>
+                  <Text style={itemHorizontal.cardTitle}>
+                    Healthy Food
+                  </Text>
+                </View>
+                <View>
+                  <View style={itemHorizontal.cardIcon}>
+                    <Receipt21
+                      color={colors.white()}
+                      variant="Linear"
+                      size={20}
+                    />
+                  </View>
+                </View>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={itemHorizontal.cardItem}>
+            <ImageBackground
+              style={itemHorizontal.cardImage}
+              resizeMode="cover"
+              imageStyle={{borderRadius: 15}}
+              source={{
+                uri: 'https://www.astronauts.id/blog/wp-content/uploads/2022/10/Ikuti-Resep-Menu-Diet-Sehat-30-Hari-Ini-yang-Aman-dan-Enak-1024x683.jpg',
+              }}>
+              <View style={itemHorizontal.cardContent}>
+                <View style={itemHorizontal.cardInfo}>
+                  <Text style={itemHorizontal.cardTitle}>Diet Sehat</Text>
+                </View>
+                <View>
+                  <View style={itemHorizontal.cardIcon}>
+                    <Receipt21
+                      color={colors.white()}
+                      variant="Linear"
+                      size={20}
+                    />
+                  </View>
+                </View>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={itemHorizontal.cardItem}>
+            <ImageBackground
+              style={itemHorizontal.cardImage}
+              resizeMode="cover"
+              imageStyle={{borderRadius: 15}}
+              source={{
+                uri: 'https://asset.kompas.com/crops/8i4IWzWN2Qu9FRNGNhfvHu60q1I=/0x457:4500x3457/750x500/data/photo/2022/10/25/63574d0f01f62.jpg',
+              }}>
+              <View style={itemHorizontal.cardContent}>
+                <View style={itemHorizontal.cardInfo}>
+                  <Text style={itemHorizontal.cardTitle}>Hidup Sehat</Text>
+                </View>
+                <View>
+                  <View style={itemHorizontal.cardIcon}>
+                    <Receipt21
+                      color={colors.white()}
+                      variant="Linear"
+                      size={20}
+                    />
+                  </View>
+                </View>
+              </View>
+            </ImageBackground>
+          </View>
+        </ScrollView>
+        <View style={itemVertical.listCard}>
+          <View style={itemVertical.cardItem}>
+            <Image
+              style={itemVertical.cardImage}
+              source={{
+                uri: 'https://tse4.mm.bing.net/th?id=OIP.u1hfDbji5A_VudzjBDBjbQHaEK&pid=Api&P=0&h=180',
+              }}
+            />
+            <View style={itemVertical.cardContent}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <View style={{gap: 5, width: '70%'}}>
+                  <Text style={itemVertical.cardTitle}>Asam Lambung</Text>
+                  <Text style={itemVertical.cardTitle}>Terjadi ketika cairan lambung naik ke kerongkongan</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          
+          <View style={itemVertical.cardItem}>
+            <Image
+              style={itemVertical.cardImage}
+              source={{
+                uri: 'https://infotangerang.co.id/wp-content/uploads/2023/03/Illustrasi-Diare.jpg',
+              }}
+            />
+            <View style={itemVertical.cardContent}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <View style={{gap: 5, width: '70%'}}>
+                  <Text style={itemVertical.cardTitle}>Diare</Text>
+                  <Text style={itemVertical.cardTitle}>kondisi ketika buang air besar menjadi lebih sering , biasanya disebabkan oleh infeksi</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={itemVertical.cardItem}>
+            <Image
+              style={itemVertical.cardImage}
+              source={{
+                uri: 'https://png.pngtree.com/png-vector/20221227/ourlarge/pngtree-measuring-blood-pressure-and-healthcare-concept-png-image_6540079.png',
+              }}
+            />
+            <View style={itemVertical.cardContent}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <View style={{gap: 5, width: '70%'}}>
+                  <Text style={itemVertical.cardTitle}>Darah Rendah</Text>
+                  <Text style={itemVertical.cardTitle}>kondisi ketika tekanan darah turun terlalu rendah, yang bisa menyebabkan pusing</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={itemVertical.cardItem}>
+            <Image
+              style={itemVertical.cardImage}
+              source={{
+                uri: 'https://tse1.mm.bing.net/th?id=OIP.pDjiylE8Ea9iJrdWCMwOzgHaE5&pid=Api&P=0&h=180',
+              }}
+            />
+            <View style={itemVertical.cardContent}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <View style={{gap: 5, width: '70%'}}>
+                  <Text style={itemVertical.cardTitle}>Obesitas</Text>
+                  <Text style={itemVertical.cardTitle}>kondisi kelebihan berat badan yang disebabkan oleh penumpukan lemak berlebih dalam tubuh</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={itemVertical.cardItem}>
+            <Image
+              style={itemVertical.cardImage}
+              source={{
+                uri: 'https://static.republika.co.id/uploads/images/inpicture_slide/polifagia-gejala-awal-diabetes_221206220618-113.jpg',
+              }}
+            />
+            <View style={itemVertical.cardContent}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <View style={{gap: 5, width: '70%'}}>
+                  <Text style={itemVertical.cardTitle}>Diabetes</Text>
+                  <Text style={itemVertical.cardTitle}>penyakit yang terjadi ketika tubuh tidak dapat mengatur kadar gula darah dengan baik</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const itemVertical = StyleSheet.create({
+  listCard: {
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    gap: 15,
+  },
+  cardItem: {
+    backgroundColor: colors.blue(0.03),
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
+  cardCategory: {
+    color: colors.blue(),
+    fontSize: 10,
+    fontFamily: fontType['Pjs-SemiBold'],
+  },
+  cardTitle: {
+    fontSize: 14,
+    fontFamily: fontType['Pjs-Bold'],
+    color: colors.black(),
+  },
+  cardText: {
+    fontSize: 10,
+    fontFamily: fontType['Pjs-Medium'],
+    color: colors.blue(0.6),
+  },
+  cardImage: {
+    width: 94,
+    height: 94,
+    borderRadius: 10,
+    resizeMode: 'cover',
+  },
+  cardInfo: {
+    flexDirection: 'row',
+    gap: 5,
+    alignItems: 'center',
+  },
+  cardContent: {
+    gap: 10,
+    justifyContent: 'space-between',
+    paddingRight: 10,
+    paddingLeft: 15,
+    flex: 1,
+    paddingVertical: 10,
+    backgroundColor: '#ffffff',
+  },
+});
+
+const itemHorizontal = StyleSheet.create({
+  cardItem: {
+    width: 280,
+  },
+  cardImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 5,
+  },
+  cardContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+  },
+  cardInfo: {
+    justifyContent: 'flex-end',
+    height: '100%',
+    gap: 10,
+    maxWidth: '60%',
+  },
+  cardTitle: {
+    fontFamily: fontType['Pjs-Bold'],
+    fontSize: 14,
+    color: colors.white(),
+  },
+  cardText: {
+    fontSize: 10,
+    color: colors.white(),
+    fontFamily: fontType['Pjs-Medium'],
+  },
+  cardIcon: {
+    backgroundColor: colors.white(0.33),
+    padding: 5,
+    borderColor: colors.white(),
+    borderWidth: 0.5,
+    borderRadius: 5,
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#edf0ef',
+  },
+  header: {
+    paddingHorizontal: 24,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 52,
+    elevation: 8,
+    paddingTop: 8,
+    paddingBottom: 4,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fontType['Pjs-ExtraBold'],
+    color: colors.black(),
+  },
+  listCategory: {
+    paddingVertical: 10,
+  },
+  listBlog: {
+    paddingVertical: 10,
+    gap: 10,
+  },
+});
+
+const category = StyleSheet.create({
+  item: {
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    borderRadius: 25,
+    alignItems: 'center',
+    backgroundColor: colors.grey(0.08),
+    marginHorizontal: 5,
+  },
+  title: {
+    fontFamily: fontType['Pjs-SemiBold'],
+    fontSize: 14,
+    lineHeight: 18,
+    color: colors.grey(),
+  },
+});
