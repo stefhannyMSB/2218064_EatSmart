0dfb70b32a6937a7dba814dd7a601e44ebc523bc
stefhannyMSB <stefannyabel@gmail.com>
2025-05-18 19:45:22 +0700

    bab4fix

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
-// import * as React from 'react';
-// import {Discover} from './src/screens';
-// export default function App() {
-//   return <Discover />;
-// }
-
 import * as React from 'react';
-import {Profile} from './src/screens';
+import {Discover} from './src/screens';
 export default function App() {
-  return <Profile />;
+  return <Discover />;
 }
+
+// import * as React from 'react';
+// import {Profile} from './src/screens';
+// export default function App() {
+//   return <Profile />;
+// }
--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,5 +1,4 @@
 import Home from "./Home";
 import Discover from "./Discover";
-import Bookmark from "./Bookmark";
 import Profile from "./Profile";
-export {Home, Discover, Bookmark, Profile}
+export {Home, Discover, Profile}
